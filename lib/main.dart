3eb1535a65a6a6f68aa668bda55cc818013775c6
gjr <g.jruiz17@gmail.com>
2021-10-05 20:13:52 -0300

    se solucion conflicto

--- lib/main.dart
+++ lib/main.dart
@@ -24,11 +24,7 @@ class MyApp extends StatelessWidget {
           // Notice that the counter didn't reset back to zero; the application
           // is not restarted.
           primarySwatch: Colors.red),
-<<<<<<< HEAD
       home: const MyHomePage(title: 'LAPASSSSS1111111'),
-=======
-      home: const MyHomePage(title: 'LAPA'),
->>>>>>> appBar
     );
   }
 }
