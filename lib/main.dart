59196c150a5aa918275bafbf5137afd018384627
gjr <g.jruiz17@gmail.com>
2021-10-06 14:16:38 -0300

    Se cambio el nombre de la pag por Contador1

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'CONTADOR PRUEBA',
+      title: 'CONTADOR1',
       theme: ThemeData(
           // This is the theme of your application.
           //
